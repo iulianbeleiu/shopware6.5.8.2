45792bfc25890da3e84a011c8b2213f7d86f06f7
Iulian Beleiu <iulian.beleiu@gmail.com>
2024-02-07 11:45:05 +0200

    argument

--- custom/static-plugins/SwagOrder/src/Command/OrderListCommand.php
+++ custom/static-plugins/SwagOrder/src/Command/OrderListCommand.php
@@ -5,14 +5,18 @@ namespace Swag\Order\Command;
 use Shopware\Core\Framework\Context;
 use Swag\Order\Service\OrderExporterInterface;
 use Swag\Order\Service\OrderPrinterInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(
+    name: 'order:recent-orders',
+    description: 'Returns an order table with its order number & date, the name of the customer and the total amount',
+)]
 class OrderListCommand extends Command
 {
-    protected static $defaultName = 'order:recent-orders';
-
     public function __construct(
         private readonly OrderExporterInterface $orderExporter,
         private readonly OrderPrinterInterface $orderPrinter
@@ -22,12 +26,13 @@ class OrderListCommand extends Command
 
     protected function configure(): void
     {
-        $this->setDescription('Returns an order table with its order number & date, the name of the customer and the total amount');
+        $this->addArgument('number-of-days', InputArgument::REQUIRED, 'Number of days to display the order');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $orders = $this->orderExporter->export(7, Context::createDefaultContext());
+        $numberOfDays = (int) $input->getArgument('number-of-days');
+        $orders = $this->orderExporter->export($numberOfDays, Context::createDefaultContext());
 
         $this->orderPrinter->print($orders, $output);
 
